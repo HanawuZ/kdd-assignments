f01546a7a25d6127cf4524ac49e65084b2fe03f0
HanawuZ <b6302245@gmail.com>
2022-11-24 16:53:29 +0700

    add apirori test file and update apply rule function.

---
+++ assingment5/apriori_test.py
@@ -0,0 +1,19 @@
+dataset = [['Milk', 'Onion', 'Nutmeg', 'Kidney Beans', 'Eggs', 'Yogurt'],
+           ['Dill', 'Onion', 'Nutmeg', 'Kidney Beans', 'Eggs', 'Yogurt'],
+           ['Milk', 'Apple', 'Kidney Beans', 'Eggs'],
+           ['Milk', 'Unicorn', 'Corn', 'Kidney Beans', 'Yogurt'],
+           ['Corn', 'Onion', 'Onion', 'Kidney Beans', 'Ice cream', 'Eggs']]
+
+import pandas as pd
+from mlxtend.preprocessing import TransactionEncoder
+
+te = TransactionEncoder()
+te_ary = te.fit(dataset).transform(dataset)
+df = pd.DataFrame(te_ary, columns=te.columns_)
+print(df)
+
+from mlxtend.frequent_patterns import apriori
+
+frequent_itemsets = apriori(df, min_support=0.6, use_colnames=True)
+frequent_itemsets['length'] = frequent_itemsets['itemsets'].apply(lambda x: len(x))
+print(frequent_itemsets)
